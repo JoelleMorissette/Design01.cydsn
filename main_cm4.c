4032a510674ccee3d74bcb0e30ce0837afd60525
JoelleMorissette <joelle.morissette@polymtl.ca>
2021-03-19 10:22:34 -0400

    Partie 3 fonctionnel et bonus

--- main_cm4.c
+++ main_cm4.c
@@ -29,24 +29,30 @@
 //        Cy_GPIO_Write(Pin_1_0_PORT,Pin_1_0_NUM,1);
 //    }
 //}
+
 volatile SemaphoreHandle_t bouton_semph;
+volatile QueueHandle_t print_queue;
+volatile int nombreEntree = 0; 
 
 void isr_bouton(void){
-     Cy_SysPm_PmicUnlock();
-     xSemaphoreGiveFromISR(bouton_semph,NULL);
+    //Cy_SysPm_PmicUnlock();
+    nombreEntree = nombreEntree +1;
+    xSemaphoreGiveFromISR(bouton_semph,NULL);
     Cy_GPIO_ClearInterrupt(BOUTON_0_PORT, BOUTON_0_NUM);
     NVIC_ClearPendingIRQ(bouton_isr_cfg.intrSrc);
+  
    
 }
 
 void bouton_task(){
     for(;;){
+        
         xSemaphoreTake(bouton_semph,0) ;
         if( xSemaphoreTake(bouton_semph,0)== 1){
             UART_1_PutString("\r" "Bouton appuye" "\n");
             vTaskDelay(pdMS_TO_TICKS(20));
-            UART_1_PutString("\r" "Bouton relache" "\n");
-        }           
+            UART_1_PutString("\r""Bouton relache""\n");
+                  }                                         
     }
 }
 
@@ -63,36 +69,47 @@ task_params_t task_B = {
 };
 
 
-void print_loop(void * params){
+void print_loop(void* params){
     for(;;){
-       
-        UART_1_PutString(params);     
+        int delais =0;
+        char *message = NULL;
+        if(params == &task_A){
+            message = task_A.message;
+            delais = task_A.delay;
+        }
+        if(params == &task_B){
+            message = task_B.message;
+            delais = task_B.delay;
+        }
+        vTaskDelay(delais);
+        xQueueSend(print_queue, &message, delais);
     }
 }
-volatile void* pointeurA = &task_A;  // Definition pointeur vers adresse de task_A
-volatile void* pointeurB = &task_B;  // Definition pointeur vers adresse de task B 
 
 void print_loopDeux(void *params){
-    for(;;){       
-        if (params == pointeurA){      // Si l'adresse pointee par params est celle de task_A
-            UART_1_PutString(task_A.message);  // On affiche le message de task_A
-            vTaskDelay(task_A.delay);  // La periode est donc du delais de la task_A 
+    for(;;){    
+        task_params_t* paramTemporaire = (task_params_t*)params;
+        vTaskDelay(pdMS_TO_TICKS(paramTemporaire->delay));
+        UART_1_PutString(paramTemporaire->message);
             
-        }
-        if (params == pointeurB){   // Meme chose, mais avec task_B 
-           UART_1_PutString(task_B.message);
-           vTaskDelay(task_B.delay); 
-        }
-       
     }
 }
+
+void print(){
+    char*message;
+    for(;;){
+        xQueueReceive(print_queue, &message, portMAX_DELAY);
+        UART_1_PutString(message);
+    }
+}
+
 
 int main(void)
 {  
     UART_1_Start(); // Demarre le module UART
     
     bouton_semph = xSemaphoreCreateBinary(); // Initialisation du semaphore
-   
+    print_queue = xQueueCreate(2, sizeof(char*));
     // Initialisation de l'isr
     //Cy_SysPm_PmicUnlock(); // Fonction trouvee dans les recherches qui permet a l'isr de se produire 
     Cy_SysInt_Init(&bouton_isr_cfg,isr_bouton);
@@ -105,9 +122,15 @@ int main(void)
    // Creation de taches
     
 //    xTaskCreate(vInverseLED, "red",80,NULL,2,NULL);
-    xTaskCreate(bouton_task, "Etat du bouton", 80,NULL, 1, NULL);
-    xTaskCreate(print_loopDeux,"task A", configMINIMAL_STACK_SIZE,(void *)&task_A, 1, NULL);
-    xTaskCreate(print_loopDeux,"task_B", configMINIMAL_STACK_SIZE,(void *)&task_B, 1, NULL);
+   xTaskCreate(bouton_task, "Etat du bouton", 80,NULL, 1, NULL);
+//   xTaskCreate(print_loopDeux,"task A", configMINIMAL_STACK_SIZE,(void *)&task_A, 1, NULL);
+//   xTaskCreate(print_loopDeux,"task_B", configMINIMAL_STACK_SIZE,(void *)&task_B, 1, NULL);
+ 
+    // Essai    
+    xTaskCreate(print_loop,"task A", configMINIMAL_STACK_SIZE, (void*)&task_A, 1, NULL);
+    xTaskCreate(print_loop,"task_B", configMINIMAL_STACK_SIZE,(void *)&task_B, 1, NULL);
+    xTaskCreate(print , "Task_print", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+
     vTaskStartScheduler();
     
 
